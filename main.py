81dc0fe5ecbafd9186fa632db57a9703ad5d91e7
Francesco Sacchi <francesco.sacchi@intre.it>
2023-03-21 18:06:58 +0100

    Main pipeline

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 from speech_to_text import speech_to_text_translated
+from text_to_italian import translate
 from text_to_summary import text_to_summary
 from video_to_speech import video_to_speech
+from functools import reduce
 
 video_file_name = "video.mp4"
-speech = video_to_speech(video_file_name)
-text = speech_to_text_translated(speech)
-summary = text_to_summary(text)
+# speech = video_to_speech(video_file_name)
+# text = speech_to_text_translated(speech)
+# summary = text_to_summary(text)
+# translate(summary)
+
+
+steps = [video_to_speech, speech_to_text_translated, text_to_summary, translate]
+
+def main():
+    def elaborate(prev, func):
+        intermediate_result = func(prev)
+        print("res intermedio", intermediate_result)
+        return intermediate_result
+
+    reduce(elaborate, steps, video_file_name)
+
+
+if __name__ == '__main__':
+    main()
+
+
