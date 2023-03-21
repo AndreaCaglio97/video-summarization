6255f2ee97f3cdd6d1db01eb7077e77106713123
Andrea Caglio <andrea.caglio@intre.it>
2023-03-21 15:40:53 +0100

    🚧 Add some trials on speech to text

--- speech_to_text.py
+++ speech_to_text.py
@@ -5,21 +5,21 @@ model = whisper.load_model("base")
 
 def speech_to_text(audio_file="audio.mp3"):
     # load audio and pad/trim it to fit 30 seconds
-    # audio = whisper.load_audio(audio_file)
+    audio = whisper.load_audio(audio_file)
     # audio = whisper.pad_or_trim(audio)
 
     # make log-Mel spectrogram and move to the same device as the model
-    # mel = whisper.log_mel_spectrogram(audio).to(model.device)
+    mel = whisper.log_mel_spectrogram(audio).to(model.device)
 
     # detect the spoken language
     # _, probs = model.detect_language(mel)
     # print(f"Detected language: {max(probs, key=probs.get)}")
 
     options = whisper.DecodingOptions(language="en", without_timestamps=False, task="translate")
-    result = model.transcribe(audio_file, options)
+    result = whisper.decode(model, mel, options)
     print(result)
+    print(result["text"])
     return result["text"]
-    # print(result["text"])
 
 
 # run if this file is called directly
